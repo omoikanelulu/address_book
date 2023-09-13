eb304d7beee50b3444bff3bae0c332e1df10067b
omoikanelulu <r.omoikane@gmail.com>
2023-09-13 13:49:41 +0900

    作成するカラムを定義

--- database/migrations/2023_09_13_114242_create_addresses_table.php
+++ database/migrations/2023_09_13_114242_create_addresses_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->integer('person_id');
+            $table->string('country')->comment('国');
+            $table->string('type')->comment('住所の種類');
+            $table->integer('postal_code')->comment('郵便番号');
+            $table->string('state')->comment('都道府県');
+            $table->string('city')->comment('市町村');
+            $table->string('street_address')->nullable()->comment('その他の住所');
             $table->timestamps();
         });
     }
