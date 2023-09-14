a2ab99bbef598878f9ba5018a7827722b9ffa5c1
omoikanelulu <r.omoikane@gmail.com>
2023-09-14 15:15:33 +0900

    yield('content')にm-5が適用されるよう記述

--- resources/views/layouts/address_app.blade.php
+++ resources/views/layouts/address_app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="m-5 font-sans text-gray-900 antialiased">
             @yield('content')
         </div>
     </body>
