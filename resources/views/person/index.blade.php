e90a05e41880bc66aa02732aa8b7c207d210d9fd
omoikanelulu <r.omoikane@gmail.com>
2023-09-14 15:16:40 +0900

    簡単なスタイリングを記述

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.address_app')
 
 @section('content')
-    <a href="{{ route('person.create') }}">登録画面へ</a>
+    <a href="{{ route('person.create') }}" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+        登録画面へ
+    </a>
 
     {{-- 操作の結果を表示する --}}
     @if (session('message'))
-        <div class="bg-green-400 p-5">
+        <div class="bg-green-400 p-5 mt-5 rounted">
             <p>{{ session('message') }}</p>
         </div>
     @endif
 
-    <table>
+    <table class="mx-auto mt-5 w-11/12 border">
         <thead>
-            <tr>
-                <th>ID</th>
-                <th>姓</th>
-                <th>名</th>
-                <th>メールアドレス</th>
-                <th>電話番号</th>
-                <th>生年月日</th>
-                <th>操作</th>
+            <tr class="bg-gray-500 text-white">
+                <th class="text-left">ID</th>
+                <th class="text-left">姓</th>
+                <th class="text-left">名</th>
+                <th class="text-left">メールアドレス</th>
+                <th class="text-left">電話番号</th>
+                <th class="text-left">生年月日</th>
+                <th class="text-left">操作</th>
             </tr>
         </thead>
         <tbody>
@@ -35,9 +37,12 @@
 
                     {{-- 操作ボタン群 --}}
                     <td>
-                        <a href="{{ route('person.show', $person->id) }}">詳細</a>
-                        <a href="{{ route('person.edit', $person->id) }}">編集</a>
-                        <form action="{{ route('person.destroy', $person->id) }}" method="post">
+                        <a class="inline-block p-2 bg-blue-100 hover:bg-blue-200 rounded"
+                            href="{{ route('person.show', $person->id) }}">詳細</a>
+                        <a class="inline-block p-2 bg-yellow-100 hover:bg-yellow-200 rounded"
+                            href="{{ route('person.edit', $person->id) }}">編集</a>
+                        <form class="inline-block p-2 bg-red-100 hover:bg-red-200 rounded"
+                            action="{{ route('person.destroy', $person->id) }}" method="post">
                             @csrf
                             @method('DELETE')
                             <input type="submit" value="削除">
