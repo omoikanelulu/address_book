e02e3147053d3d955b55542d180f6eedbcb1e4bf
omoikanelulu <r.omoikane@gmail.com>
2023-09-18 15:06:15 +0900

    ダッシュボードへのrouteを削除、リソースコントローラを、認証ミドルウェア内に記述

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// リソースコントローラの記述
-Route::resource('address', AddressController::class);
-Route::resource('person', PersonController::class);
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // リソースコントローラの記述
+    Route::resource('address', AddressController::class);
+    Route::resource('person', PersonController::class);
 });
