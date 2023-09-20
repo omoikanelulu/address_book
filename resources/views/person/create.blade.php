23fabf3476d9f23617b8a44dca405905e55670fd
omoikanelulu <r.omoikane@gmail.com>
2023-09-20 12:41:53 +0900

    addressとpersonに登録するようフォームを作成

--- resources/views/person/create.blade.php
+++ resources/views/person/create.blade.php
@@ -1,20 +1,62 @@
 @extends('layouts.address_app')
 
 @section('content')
-    <a href="{{ route('person.index') }}">一覧画面へ</a>
+    <a class="btn btn-primary mb-4" href="{{ route('person.index') }}">一覧画面へ</a>
 
     <form action="{{ route('person.store') }}" method="post">
         @csrf
-        <label for="last_name">姓</label>
-        <input type="text" name="last_name" id="last_name">
-        <label for="first_name">名</label>
-        <input type="text" name="first_name" id="first_name">
-        <label for="email_address">メールアドレス</label>
-        <input type="email" name="email_address" id="email_address">
-        <label for="phone_number">電話番号</label>
-        <input type="tel" name="phone_number" id="phone_number">
-        <label for="birth_date">生年月日</label>
-        <input type="date" name="birth_date" id="birth_date">
-        <input type="submit" value="登録する">
+        <div class="row mb-4">
+            <div class="col-6">
+                <label class="form-label col-6" for="last_name">姓</label>
+                <input class="form-control col-6 rounded" type="text" name="last_name" id="last_name">
+            </div>
+            <div class="col-6">
+                <label class="form-label col-6" for="first_name">名</label>
+                <input class="form-control col-6 rounded" type="text" name="first_name" id="first_name">
+            </div>
+            <div class="col">
+                <label class="form-label" for="email_address">メールアドレス</label>
+                <input class="form-control rounded" type="email" name="email_address" id="email_address">
+            </div>
+            <div class="col">
+                <label class="form-label" for="phone_number">電話番号</label>
+                <input class="form-control rounded" type="tel" name="phone_number" id="phone_number">
+            </div>
+            <div class="col">
+                <label class="form-label" for="birth_date">生年月日</label>
+                <input class="form-control rounded" type="date" name="birth_date" id="birth_date">
+            </div>
+        </div>
+
+        <div class="row mb-4">
+            <div class="col-4">
+                <label class="form-label" for="country">国</label>
+                <input class="form-control rounded" type="text" name="country" id="country">
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="type">種類</label>
+                <input class="form-control rounded" type="text" name="type" id="type">
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="postal_code">郵便番号</label>
+                <input class="form-control rounded" type="text" name="postal_code" id="postal_code">
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="postal_code">都道府県</label>
+                <input class="form-control rounded" type="text" name="state" id="state">
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="postal_code">市町村</label>
+                <input class="form-control rounded" type="text" name="city" id="city">
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="postal_code">その他</label>
+                <input class="form-control rounded" type="text" name="street_address" id="street_address">
+            </div>
+        </div>
+
+        {{-- ログイン中のユーザID --}}
+        <input type="hidden" name="person_id" id="person_id" value="{{ auth()->id() }}">
+        <input class="btn btn-primary bg-danger" type="submit" value="登録する">
     </form>
 @endsection
