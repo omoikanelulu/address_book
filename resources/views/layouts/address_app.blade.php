cfc28ffa0eb9f30aa1deb758987ef02b7591458c
omoikanelulu <r.omoikane@gmail.com>
2023-09-20 12:42:16 +0900

    bootstrapを導入

--- resources/views/layouts/address_app.blade.php
+++ resources/views/layouts/address_app.blade.php
@@ -12,6 +12,10 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    {{-- bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -40,12 +44,11 @@
     @stack('modals')
 
     @livewireScripts
-</body>
 
-{{-- <body>
-        <div class="m-5 font-sans text-gray-900 antialiased">
-            @yield('content')
-        </div>
-    </body> --}}
+    {{-- bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
 
 </html>
